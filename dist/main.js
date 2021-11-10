ed9b225762473388141f7e007db274dac3e509b6
jhordan13 <jor.sk13.7@gmail.com>
2021-11-10 01:57:40 -0500

    Tipos de datos number, boolean y string

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,33 @@
 "use strict";
 console.log("Soy el archivo main");
+/* ######### Tipo number ######### */
+// ---- Forma Explicita ----
+var phone;
+phone = 991211293;
+// phone = "Hola"; --> No es posible asignar una cadena a variable de tipo numerico  
+// ---- Forma Inferido -----
+var phoneNumber = 991211293;
+// phoneNumber = true; --> Error porque de forma implicita la variable "phoneNumber" es de tipo number
+var hex = 0xf00d; // El tipo "number" acepta numeros hexadecimales iniciando el numero de la forma "0xnum_hexadecimal"
+var binary = 5; // El tipo "number" acepta numeros binarios iniciando el numero de la forma "0bnum_binario"
+var octal = 449; // El tipo "number" acepta numeros octales iniciando el numero de la forma "0onum_octal"
+/* ######### Tipo Boolean ######### */
+var isPro = true; // Declaracion explicita
+var isUserPro = false; //Declaracion inferida
+// isUserPro = 234; --> Error porque de forma implicita la variable "isUserPro" es de tipo boolean
+/* ######### Tipo String ######### */
+var userName = "Jordan13"; // Declaracion explicita
+userName = "Paul13";
+// userName = true; --> Error por ser de tipo string
+var myname = "Jordan";
+/*
+###########################################
+#           Template String
+############################################
+* Permiten definir multiples lineas de texto
+* Pueden contener expresiones o variables embebidas
+* Se debe usar el caracter backtick/backquote(`) y para expresiones ${expr}
+*/
+var userInfo;
+userInfo = "\n  User info: \n  username: " + userName + "\n  firstName: " + (myname + " Perez") + "\n  phone: " + phoneNumber + "\n  isPro: " + isPro + "\n";
+console.log("userInfo: ", userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,51 @@
 console.log("Soy el archivo main");
+
+/* ######### Tipo number ######### */
+
+// ---- Forma Explicita ----
+let phone : number;
+phone = 991211293;
+// phone = "Hola"; --> No es posible asignar una cadena a variable de tipo numerico  
+
+// ---- Forma Inferido -----
+let phoneNumber = 991211293;
+// phoneNumber = true; --> Error porque de forma implicita la variable "phoneNumber" es de tipo number
+
+let hex : number = 0xf00d; // El tipo "number" acepta numeros hexadecimales iniciando el numero de la forma "0xnum_hexadecimal"
+let binary : number = 0b101; // El tipo "number" acepta numeros binarios iniciando el numero de la forma "0bnum_binario"
+let octal : number = 0o701; // El tipo "number" acepta numeros octales iniciando el numero de la forma "0onum_octal"
+
+/* ######### Tipo Boolean ######### */
+
+let isPro : boolean = true; // Declaracion explicita
+let  isUserPro = false;     //Declaracion inferida
+// isUserPro = 234; --> Error porque de forma implicita la variable "isUserPro" es de tipo boolean
+
+/* ######### Tipo String ######### */
+
+let userName : string = "Jordan13"; // Declaracion explicita
+userName = "Paul13";
+// userName = true; --> Error por ser de tipo string
+
+let  myname = "Jordan";
+
+/* 
+###########################################
+#           Template String   
+############################################ 
+* Permiten definir multiples lineas de texto
+* Pueden contener expresiones o variables embebidas
+* Se debe usar el caracter backtick/backquote(`) y para expresiones ${expr}
+*/
+
+let userInfo : string;
+
+userInfo = `
+  User info: 
+  username: ${userName}
+  firstName: ${myname+" Perez"}
+  phone: ${phoneNumber}
+  isPro: ${isPro}
+`;
+
+console.log("userInfo: ",userInfo);
